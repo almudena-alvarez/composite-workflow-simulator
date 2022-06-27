1254885cfe4508e28def61cec8bdbbc2eade0d96
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-06-27 12:21:30 +0200

    Update checkvariable.sh

--- scripts/checkvariable.sh
+++ scripts/checkvariable.sh
@@ -1,6 +1,7 @@
-response=["organizations": "lalala"]
+response=[{"organizations": "lalala"}]
 
 if [[ $response == "[]" ]] 
 then
      ${{ env.CHECKING_DELETION }} = true
+     echo ${{ env.CHECKING_DELETION }}
 fi
