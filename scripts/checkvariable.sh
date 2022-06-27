00fc63169e738b65e3b064bfc78fb6cc64a5b6b4
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-06-27 12:36:44 +0200

    Update checkvariable.sh

--- scripts/checkvariable.sh
+++ scripts/checkvariable.sh
@@ -1,8 +1,9 @@
-response=[{"organizations": "lalala"}]
+#!/bin/bash
+response=$(curl https://reqbin.com/echo/get/json -H "Accept: application/json")
 
 if [[ $response != "[]" ]] 
 then
-     ${{ env.CHECKING_DELETION }} = true
+#      ${{ env.CHECKING_DELETION }} = true
      echo ${{ env.CHECKING_DELETION }}
      echo "Game over!"
      exit 1
