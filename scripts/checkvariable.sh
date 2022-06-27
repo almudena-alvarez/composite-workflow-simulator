df364d0b8eda8fc350b73297d979286d4590c63a
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-06-27 12:33:37 +0200

    Update checkvariable.sh

--- scripts/checkvariable.sh
+++ scripts/checkvariable.sh
@@ -1,7 +1,9 @@
 response=[{"organizations": "lalala"}]
 
-if [[ $response == "[]" ]] 
+if [[ $response != "[]" ]] 
 then
      ${{ env.CHECKING_DELETION }} = true
      echo ${{ env.CHECKING_DELETION }}
+     echo "Game over!"
+     exit 1
 fi
