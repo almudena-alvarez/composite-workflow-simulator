0c189b5e7ed7b726b15424de644066147e30ab59
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-10 18:48:03 +0200

    Update scriptarray.sh

--- scripts/scriptarray.sh
+++ scripts/scriptarray.sh
@@ -1,43 +1,43 @@
 #!/usr/bin/env bash
 
-# function input_parameters() {
-#   local p_alm_users=""
+function input_parameters() {
+  local p_alm_users=""
 
-# while [[ $# -gt 0 ]]; do
-#       case "$1" in
-#           -u|--users)
-#               if [[ $# == 1 ]]; then
-#                 >&2 echo "ERROR: No users specified"
-#                 exit 1
-#               fi
-#               p_alm_users="${2}"
-#               shift 2
-#               ;;
+while [[ $# -gt 0 ]]; do
+      case "$1" in
+          -u|--users)
+              if [[ $# == 1 ]]; then
+                >&2 echo "ERROR: No users specified"
+                exit 1
+              fi
+              p_alm_users="${2}"
+              shift 2
+              ;;
           
-#           --)
-#               shift
-#               break
-#               ;;
-#           -*)
-#               >&2 echo "ERROR: Unexpected option '${1}'"
-#               exit 1
-#               ;;
-#           *)
-#               break
-#               ;;
-#       esac
-#   done
+          --)
+              shift
+              break
+              ;;
+          -*)
+              >&2 echo "ERROR: Unexpected option '${1}'"
+              exit 1
+              ;;
+          *)
+              break
+              ;;
+      esac
+  done
 
 
-#   alm_user="${p_alm_users:-${INPUT_ALM_USERS?"No ALM user defined"}}"
+  alm_user="${p_alm_users:-${INPUT_ALM_USERS?"No ALM user defined"}}"
 
 
 
-#   # echo "Creating ${target_schema} from ${source_schema} using SQL DMLs from ${script_dir}..."
-# }
+  # echo "Creating ${target_schema} from ${source_schema} using SQL DMLs from ${script_dir}..."
+}
 
 
-# input_parameters "$@"
+input_parameters "$@"
 
 # echo $alm_user
 
