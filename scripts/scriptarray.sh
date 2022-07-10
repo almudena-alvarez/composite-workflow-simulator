37bcf6ec638c9bc25150594a534c816906f1f080
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-10 19:00:04 +0200

    Update scriptarray.sh

--- scripts/scriptarray.sh
+++ scripts/scriptarray.sh
@@ -1,51 +1,48 @@
 #!/bin/bash
 
-function execute_ddl() {
-        echo "hi"
+
+function input_parameters() {
+
+echo "holiff"
+  local p_alm_users=""
+
+while [[ $# -gt 0 ]]; do
+      case "$1" in
+          -u|--users)
+              if [[ $# == 1 ]]; then
+                >&2 echo "ERROR: No users specified"
+                exit 1
+              fi
+              p_alm_users="${2}"
+              shift 2
+              ;;
+          
+          --)
+              shift
+              break
+              ;;
+          -*)
+              >&2 echo "ERROR: Unexpected option '${1}'"
+              exit 1
+              ;;
+          *)
+              break
+              ;;
+      esac
+  done
+
+
+  alm_user="${p_alm_users:-${INPUT_ALM_USERS?"No ALM user defined"}}"
+
+
+
+  # echo "Creating ${target_schema} from ${source_schema} using SQL DMLs from ${script_dir}..."
 }
 
-execute_ddl
-# function input_parameters() {
 
-# echo "holiff"
-# #   local p_alm_users=""
+input_parameters "$@"
 
-# # while [[ $# -gt 0 ]]; do
-# #       case "$1" in
-# #           -u|--users)
-# #               if [[ $# == 1 ]]; then
-# #                 >&2 echo "ERROR: No users specified"
-# #                 exit 1
-# #               fi
-# #               p_alm_users="${2}"
-# #               shift 2
-# #               ;;
-          
-# #           --)
-# #               shift
-# #               break
-# #               ;;
-# #           -*)
-# #               >&2 echo "ERROR: Unexpected option '${1}'"
-# #               exit 1
-# #               ;;
-# #           *)
-# #               break
-# #               ;;
-# #       esac
-# #   done
+echo "alm_user:"
+echo $alm_user
 
-
-# #   alm_user="${p_alm_users:-${INPUT_ALM_USERS?"No ALM user defined"}}"
-
-
-
-#   # echo "Creating ${target_schema} from ${source_schema} using SQL DMLs from ${script_dir}..."
-# }
-
-
-# input_parameters "$@"
-
-# # echo $alm_user
-
-# echo "holi"
+echo "holi"
