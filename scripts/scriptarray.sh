70eb035fc03d6cb9982df83ee5ba2a3bd2319b1c
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-10 18:48:58 +0200

    Update scriptarray.sh

--- scripts/scriptarray.sh
+++ scripts/scriptarray.sh
@@ -3,30 +3,30 @@
 function input_parameters() {
   local p_alm_users=""
 
-while [[ $# -gt 0 ]]; do
-      case "$1" in
-          -u|--users)
-              if [[ $# == 1 ]]; then
-                >&2 echo "ERROR: No users specified"
-                exit 1
-              fi
-              p_alm_users="${2}"
-              shift 2
-              ;;
+# while [[ $# -gt 0 ]]; do
+#       case "$1" in
+#           -u|--users)
+#               if [[ $# == 1 ]]; then
+#                 >&2 echo "ERROR: No users specified"
+#                 exit 1
+#               fi
+#               p_alm_users="${2}"
+#               shift 2
+#               ;;
           
-          --)
-              shift
-              break
-              ;;
-          -*)
-              >&2 echo "ERROR: Unexpected option '${1}'"
-              exit 1
-              ;;
-          *)
-              break
-              ;;
-      esac
-  done
+#           --)
+#               shift
+#               break
+#               ;;
+#           -*)
+#               >&2 echo "ERROR: Unexpected option '${1}'"
+#               exit 1
+#               ;;
+#           *)
+#               break
+#               ;;
+#       esac
+#   done
 
 
   alm_user="${p_alm_users:-${INPUT_ALM_USERS?"No ALM user defined"}}"
