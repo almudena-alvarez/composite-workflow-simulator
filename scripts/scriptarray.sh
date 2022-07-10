d21a102dc01aa0eecf56c56a16e5fed6b39a637f
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-10 18:55:14 +0200

    Update scriptarray.sh

--- scripts/scriptarray.sh
+++ scripts/scriptarray.sh
@@ -1,46 +1,51 @@
 #!/usr/bin/env bash
 
-function input_parameters() {
-
-echo "holiff"
-#   local p_alm_users=""
-
-# while [[ $# -gt 0 ]]; do
-#       case "$1" in
-#           -u|--users)
-#               if [[ $# == 1 ]]; then
-#                 >&2 echo "ERROR: No users specified"
-#                 exit 1
-#               fi
-#               p_alm_users="${2}"
-#               shift 2
-#               ;;
-          
-#           --)
-#               shift
-#               break
-#               ;;
-#           -*)
-#               >&2 echo "ERROR: Unexpected option '${1}'"
-#               exit 1
-#               ;;
-#           *)
-#               break
-#               ;;
-#       esac
-#   done
-
-
-#   alm_user="${p_alm_users:-${INPUT_ALM_USERS?"No ALM user defined"}}"
-
-
-
-  # echo "Creating ${target_schema} from ${source_schema} using SQL DMLs from ${script_dir}..."
+function execute_ddl() {
+        echo "hi"
 }
 
+execute_ddl
+# function input_parameters() {
 
-input_parameters "$@"
+# echo "holiff"
+# #   local p_alm_users=""
 
-# echo $alm_user
+# # while [[ $# -gt 0 ]]; do
+# #       case "$1" in
+# #           -u|--users)
+# #               if [[ $# == 1 ]]; then
+# #                 >&2 echo "ERROR: No users specified"
+# #                 exit 1
+# #               fi
+# #               p_alm_users="${2}"
+# #               shift 2
+# #               ;;
+          
+# #           --)
+# #               shift
+# #               break
+# #               ;;
+# #           -*)
+# #               >&2 echo "ERROR: Unexpected option '${1}'"
+# #               exit 1
+# #               ;;
+# #           *)
+# #               break
+# #               ;;
+# #       esac
+# #   done
 
-echo "holi"
+
+# #   alm_user="${p_alm_users:-${INPUT_ALM_USERS?"No ALM user defined"}}"
+
+
+
+#   # echo "Creating ${target_schema} from ${source_schema} using SQL DMLs from ${script_dir}..."
+# }
+
+
+# input_parameters "$@"
+
+# # echo $alm_user
+
+# echo "holi"
