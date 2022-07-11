a54d0891751fbbb82fd9f7fed69ea4556fbb6bc2
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-11 13:37:47 +0200

    Update postgres-select.sh

--- scripts/postgres-select.sh
+++ scripts/postgres-select.sh
@@ -18,8 +18,29 @@ echo $master
 # echo $psql_array
 
 jeje=($(psql -h postgres -d postgres_db -U postgres_user -AXqtc "SELECT tablename FROM pg_tables WHERE schemaname = 'esq_oasis' AND tablename NOT IN  ('first')"));
+echo "printing jjee"
 echo $jeje
-bis=`psql -h postgres -d postgres_db -U postgres_user -AXqtc "SELECT tablename FROM pg_tables WHERE schemaname = 'esq_oasis' AND tablename NOT IN  ('first')"`
+
+
+for z in "${jeje[@]}"
+    do
+    : 
+    echo "ejej in looop"
+    echo "$z"
+done;
+
+IFS=' ' read -r -a arrayjeje <<< "$jeje"
+echo "jejejejejejejejeje"
+echo $arrayjeje
+
+for z in "${arrayjeje[@]}"
+    do
+    : 
+    echo "arrayjeje in looop"
+    echo "$z"
+done;
+
+bis=`psql -h postgres -d postgres_db -U postgres_user -AXqtc "SELECT tablename FROM pg_tables WHERE schemaname = 'esq_oasis' AND tablename NOT IN  ('second')"`
 
 echo "bis"
 echo $bis
@@ -38,32 +59,52 @@ done;
 echo "not in loop anymore"
 # read -r -a psarray <<< $bis
 
-var=$(`psql -h postgres -d postgres_db -U postgres_user -AXqtc "SELECT tablename FROM pg_tables WHERE schemaname = 'esq_oasis'"`);
+bis=`psql -h postgres -d postgres_db -U postgres_user -AXqtc "SELECT tablename FROM pg_tables WHERE schemaname = 'esq_oasis' AND tablename NOT IN  ('third')"`
 
+echo "bis"
+echo $bis
 
-echo "var"
-echo $var
+IFS=' ' read -r -a arraybis <<< "$bis"
+echo "arraybis"
+echo $arraybis
 
-for z in "${psarray[@]}"
+for z in "${arraybis[@]}"
     do
     : 
-    echo "bis"
+    echo "arraybis in looop"
     echo "$z"
-#     clean=$(echo "$z" | tr -d '\r')
-#     schema_table=$original_schema.$clean
-#     echo "schema_table"
-#     echo $schema_table
-#     execute_truncate $schema_table
 done;
 
-for z in "${var[@]}"
-    do
-    : 
-    echo "var"
-    echo "$z"
-#     clean=$(echo "$z" | tr -d '\r')
-#     schema_table=$original_schema.$clean
-#     echo "schema_table"
-#     echo $schema_table
-#     execute_truncate $schema_table
-done;
+echo "not in loop anymore"
+
+
+
+# var=$(`psql -h postgres -d postgres_db -U postgres_user -AXqtc "SELECT tablename FROM pg_tables WHERE schemaname = 'esq_oasis'"`);
+
+
+# echo "var"
+# echo $var
+
+# for z in "${psarray[@]}"
+#     do
+#     : 
+#     echo "bis"
+#     echo "$z"
+# #     clean=$(echo "$z" | tr -d '\r')
+# #     schema_table=$original_schema.$clean
+# #     echo "schema_table"
+# #     echo $schema_table
+# #     execute_truncate $schema_table
+# done;
+
+# for z in "${var[@]}"
+#     do
+#     : 
+#     echo "var"
+#     echo "$z"
+# #     clean=$(echo "$z" | tr -d '\r')
+# #     schema_table=$original_schema.$clean
+# #     echo "schema_table"
+# #     echo $schema_table
+# #     execute_truncate $schema_table
+# done;
