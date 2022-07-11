279dd5493e5469285b88735ad570116e11277f93
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-11 17:47:58 +0200

    Update postgres-setup-new-table.sql

--- scripts/postgres-setup-new-table.sql
+++ scripts/postgres-setup-new-table.sql
@@ -1,3 +1,3 @@
-ALTER SCHEMA $original_schema RENAME TO $copySchema;
+ALTER SCHEMA @original_schema RENAME TO @copySchema;
 \dn;
-CREATE SCHEMA $original_schema AUTHORIZATION $p_target_username;
+CREATE SCHEMA @original_schema AUTHORIZATION @p_target_username;
