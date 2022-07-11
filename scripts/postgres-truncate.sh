feaf8b91c3420dc75343819438e17cd68adb5891
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-11 19:02:01 +0200

    Update postgres-truncate.sh

--- scripts/postgres-truncate.sh
+++ scripts/postgres-truncate.sh
@@ -167,9 +167,9 @@ cat "$workflow_scripts"/truncate.sql
  execute_ddl truncate.sql;
  
  PGSSLMODE="${pgsslmode}" PGPASSWORD="${target_password}" psql -h ${target_host} -U ${target_username} -d ${target_database} -c '\dn'
- PGSSLMODE="${pgsslmode}" PGPASSWORD="${target_password}" psql -h ${target_host} -U ${target_username} -d ${target_database} -c 'SELECT count(*) FROM $original_schema.first'
- PGSSLMODE="${pgsslmode}" PGPASSWORD="${target_password}" psql -h ${target_host} -U ${target_username} -d ${target_database} -c 'SELECT count(*) FROM $original_schema.second'
- PGSSLMODE="${pgsslmode}" PGPASSWORD="${target_password}" psql -h ${target_host} -U ${target_username} -d ${target_database} -c 'SELECT count(*) FROM $original_schema.third'
+ PGSSLMODE="${pgsslmode}" PGPASSWORD="${target_password}" psql -h ${target_host} -U ${target_username} -d ${target_database} -c "SELECT count(*) FROM $original_schema.first"
+ PGSSLMODE="${pgsslmode}" PGPASSWORD="${target_password}" psql -h ${target_host} -U ${target_username} -d ${target_database} -c "SELECT count(*) FROM $original_schema.second"
+ PGSSLMODE="${pgsslmode}" PGPASSWORD="${target_password}" psql -h ${target_host} -U ${target_username} -d ${target_database} -c "SELECT count(*) FROM $original_schema.third"
 
 # echo "insert karate data"
 # execute_ddl -f "$workflow_scripts"/$karateDataFile
