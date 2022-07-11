df33beb7c3da0d1987a1afe14f554ed5b88ef578
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-11 14:19:53 +0200

    Create karateDataFile.sql

---
+++ scripts/karateDataFile.sql
@@ -0,0 +1,16 @@
+INSERT INTO esq_oasis.first VALUES
+(6, 'as', 'as', 'as@as.com'),
+(7, 'bc', 'bc', 'bc@bc.com'),
+(8, 'df', 'df', 'df@df.com');
+
+
+INSERT INTO esq_oasis.second VALUES
+(6, 'as', 'as', 'as@as.com'),
+(7, 'bc', 'bc', 'bc@bc.com'),
+(8, 'df', 'df', 'df@df.com');
+
+
+INSERT INTO esq_oasis.third VALUES
+(6, 'as', 'as', 'as@as.com'),
+(7, 'bc', 'bc', 'bc@bc.com'),
+(8, 'df', 'df', 'df@df.com');
