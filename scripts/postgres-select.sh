4639bff35d660120f7d2587232d54f0c5d34430c
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-11 10:56:42 +0200

    Update postgres-select.sh

--- scripts/postgres-select.sh
+++ scripts/postgres-select.sh
@@ -18,8 +18,7 @@ echo $master
 # echo $psql_array
 
 # var=($( psql -h postgres -d postgres_db -U postgres_user -AXqtc "SELECT tablename FROM pg_tables WHERE schemaname = 'esq_oasis' AND tablename NOT IN  ('first')"));
-
-var=($( psql -h postgres -d postgres_db -U postgres_user -AXqtc "SELECT tablename FROM pg_tables WHERE schemaname = 'esq_oasis'"));
+psql -h postgres -d postgres_db -U postgres_user -AXqtc "SELECT tablename FROM pg_tables WHERE schemaname = 'esq_oasis'";
 
 
 echo "var"
