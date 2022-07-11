fc5f6ca05ebcf8a0ebc3cf6bb290cd9e06b372d7
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-11 10:32:53 +0200

    Update postgres-select.sh

--- scripts/postgres-select.sh
+++ scripts/postgres-select.sh
@@ -7,8 +7,8 @@ echo $master
 # printf -v psql_array "'%s'," "${master[@]//\'/\'\'}"
 # psql_array=${psql_array%,}
 
-echo "psql_array"
-echo $psql_array
+# echo "psql_array"
+# echo $psql_array
 
 var=($( psql -U postgres -AXqtc "SELECT tablename FROM pg_tables WHERE schemaname = 'esq_oasis' AND tablename NOT IN  ($master)"));
 
