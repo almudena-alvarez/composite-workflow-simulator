eca9200e50cd81e430b4ee04e41f13d3b814564e
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-11 14:49:13 +0200

    Update postgres-setup.sql

--- scripts/postgres-setup.sql
+++ scripts/postgres-setup.sql
@@ -1,44 +1,44 @@
-CREATE SCHEMA esq_oasis AUTHORIZATION postgres_user;
+CREATE SCHEMA esq_workflow AUTHORIZATION postgres_user;
 
 
-CREATE TABLE esq_oasis.first (
+CREATE TABLE esq_workflow.first (
 	user_id serial PRIMARY KEY,
 	username VARCHAR ( 50 ) UNIQUE NOT NULL,
 	password VARCHAR ( 50 ) NOT NULL,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL
 );
 
-CREATE TABLE esq_oasis.second (
+CREATE TABLE esq_workflow.second (
 	user_id serial PRIMARY KEY,
 	username VARCHAR ( 50 ) UNIQUE NOT NULL,
 	password VARCHAR ( 50 ) NOT NULL,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL
 );
 
-CREATE TABLE esq_oasis.third (
+CREATE TABLE esq_workflow.third (
 	user_id serial PRIMARY KEY,
 	username VARCHAR ( 50 ) UNIQUE NOT NULL,
 	password VARCHAR ( 50 ) NOT NULL,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL
 );
 
-INSERT INTO esq_oasis.first VALUES
+INSERT INTO esq_workflow.first VALUES
 (0, 'as', 'as', 'as@as.com'),
 (1, 'bc', 'bc', 'bc@bc.com'),
 (2, 'df', 'df', 'df@df.com');
 
 
-INSERT INTO esq_oasis.second VALUES
+INSERT INTO esq_workflow.second VALUES
 (0, 'as', 'as', 'as@as.com'),
 (1, 'bc', 'bc', 'bc@bc.com'),
 (2, 'df', 'df', 'df@df.com');
 
 
-INSERT INTO esq_oasis.third VALUES
+INSERT INTO esq_workflow.third VALUES
 (0, 'as', 'as', 'as@as.com'),
 (1, 'bc', 'bc', 'bc@bc.com'),
 (2, 'df', 'df', 'df@df.com');
 
 
 \dn
-\dt esq_oasis.*
+\dt esq_workflow.*
