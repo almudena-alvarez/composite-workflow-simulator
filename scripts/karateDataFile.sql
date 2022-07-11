1a7f32c69c4f32f944844ed0f86d1ddf4ab1f006
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-11 18:34:49 +0200

    Update karateDataFile.sql

--- scripts/karateDataFile.sql
+++ scripts/karateDataFile.sql
@@ -1,16 +1,16 @@
-INSERT INTO :copySchema.first VALUES
+INSERT INTO :original_schema.first VALUES
 (6, 'je', 'je', 'je@as.com'),
 (7, 'ja', 'ja', 'ja@bc.com'),
 (8, 'ji', 'ji', 'ji@df.com');
 
 
-INSERT INTO :copySchema.second VALUES
+INSERT INTO :original_schema.second VALUES
 (6, 'ja', 'ja', 'ja@as.com'),
 (7, 'je', 'je', 'je@bc.com'),
 (8, 'ji', 'ji', 'ji@df.com');
 
 
-INSERT INTO :copySchema.third VALUES
+INSERT INTO :original_schema.third VALUES
 (6, 'je', 'je', 'je@as.com'),
 (7, 'ja', 'ja', 'ja@bc.com'),
 (8, 'ji', 'ji', 'ji@df.com');
