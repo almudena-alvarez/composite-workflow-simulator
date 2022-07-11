1457623c90b583e58bc8feee46b9d12f945fb90f
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-11 13:43:51 +0200

    Update postgres-truncate.sh

--- scripts/postgres-truncate.sh
+++ scripts/postgres-truncate.sh
@@ -4,7 +4,13 @@ echo "master"
 echo $master
 
 
-var=($(psql -h postgres -d postgres_db -U postgres_user -AXqtc "SELECT tablename FROM pg_tables WHERE schemaname = 'esq_oasis' AND tablename NOT IN  ('first')"));
+IFS=',' read -r -a array <<< "$master"
+echo $array
+printf -v psql_array "'%s'," "${array[@]//\'/\'\'}"
+psql_array=${psql_array%,}
+
+
+var=($(psql -h postgres -d postgres_db -U postgres_user -AXqtc "SELECT tablename FROM pg_tables WHERE schemaname = 'esq_oasis' AND tablename NOT IN  ($psql_array)"));
 
 echo "loop"
 for z in "${var[@]}"
