a861e5d1788f85c123140f060ae4d57037963add
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-11 17:48:41 +0200

    Update karateDataFile.sql

--- scripts/karateDataFile.sql
+++ scripts/karateDataFile.sql
@@ -1,16 +1,16 @@
 INSERT INTO esq_almu_dena.first VALUES
-(6, 'as', 'as', 'as@as.com'),
-(7, 'bc', 'bc', 'bc@bc.com'),
-(8, 'df', 'df', 'df@df.com');
+(6, 'je', 'as', 'as@as.com'),
+(7, 'ja', 'bc', 'bc@bc.com'),
+(8, 'ji', 'df', 'df@df.com');
 
 
 INSERT INTO esq_almu_dena.second VALUES
-(6, 'as', 'as', 'as@as.com'),
-(7, 'bc', 'bc', 'bc@bc.com'),
-(8, 'df', 'df', 'df@df.com');
+(6, 'ja', 'as', 'as@as.com'),
+(7, 'je', 'bc', 'bc@bc.com'),
+(8, 'ji', 'df', 'df@df.com');
 
 
 INSERT INTO esq_almu_dena.third VALUES
-(6, 'as', 'as', 'as@as.com'),
-(7, 'bc', 'bc', 'bc@bc.com'),
-(8, 'df', 'df', 'df@df.com');
+(6, 'ja', 'as', 'as@as.com'),
+(7, 'je', 'bc', 'bc@bc.com'),
+(8, 'ji', 'df', 'df@df.com');
