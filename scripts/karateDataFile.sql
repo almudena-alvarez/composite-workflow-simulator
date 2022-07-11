ed3227de1c627df53fc4d706c590e61935c943a7
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-11 14:50:18 +0200

    Update karateDataFile.sql

--- scripts/karateDataFile.sql
+++ scripts/karateDataFile.sql
@@ -1,16 +1,16 @@
-INSERT INTO esq_oasis.first VALUES
+INSERT INTO esq_almu_dena.first VALUES
 (6, 'as', 'as', 'as@as.com'),
 (7, 'bc', 'bc', 'bc@bc.com'),
 (8, 'df', 'df', 'df@df.com');
 
 
-INSERT INTO esq_oasis.second VALUES
+INSERT INTO esq_almu_dena.second VALUES
 (6, 'as', 'as', 'as@as.com'),
 (7, 'bc', 'bc', 'bc@bc.com'),
 (8, 'df', 'df', 'df@df.com');
 
 
-INSERT INTO esq_oasis.third VALUES
+INSERT INTO esq_almu_dena.third VALUES
 (6, 'as', 'as', 'as@as.com'),
 (7, 'bc', 'bc', 'bc@bc.com'),
 (8, 'df', 'df', 'df@df.com');
