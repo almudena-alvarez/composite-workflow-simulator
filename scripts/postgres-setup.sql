f9d732cbcf906b5dbda1ce5d5dfb48f694f90196
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-11 14:49:47 +0200

    Update postgres-setup.sql

--- scripts/postgres-setup.sql
+++ scripts/postgres-setup.sql
@@ -1,44 +1,44 @@
-CREATE SCHEMA esq_workflow AUTHORIZATION postgres_user;
+CREATE SCHEMA esq_almu_dena AUTHORIZATION postgres_user;
 
 
-CREATE TABLE esq_workflow.first (
+CREATE TABLE esq_almu_dena.first (
 	user_id serial PRIMARY KEY,
 	username VARCHAR ( 50 ) UNIQUE NOT NULL,
 	password VARCHAR ( 50 ) NOT NULL,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL
 );
 
-CREATE TABLE esq_workflow.second (
+CREATE TABLE esq_almu_dena.second (
 	user_id serial PRIMARY KEY,
 	username VARCHAR ( 50 ) UNIQUE NOT NULL,
 	password VARCHAR ( 50 ) NOT NULL,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL
 );
 
-CREATE TABLE esq_workflow.third (
+CREATE TABLE esq_almu_dena.third (
 	user_id serial PRIMARY KEY,
 	username VARCHAR ( 50 ) UNIQUE NOT NULL,
 	password VARCHAR ( 50 ) NOT NULL,
 	email VARCHAR ( 255 ) UNIQUE NOT NULL
 );
 
-INSERT INTO esq_workflow.first VALUES
+INSERT INTO esq_almu_dena.first VALUES
 (0, 'as', 'as', 'as@as.com'),
 (1, 'bc', 'bc', 'bc@bc.com'),
 (2, 'df', 'df', 'df@df.com');
 
 
-INSERT INTO esq_workflow.second VALUES
+INSERT INTO esq_almu_dena.second VALUES
 (0, 'as', 'as', 'as@as.com'),
 (1, 'bc', 'bc', 'bc@bc.com'),
 (2, 'df', 'df', 'df@df.com');
 
 
-INSERT INTO esq_workflow.third VALUES
+INSERT INTO esq_almu_dena.third VALUES
 (0, 'as', 'as', 'as@as.com'),
 (1, 'bc', 'bc', 'bc@bc.com'),
 (2, 'df', 'df', 'df@df.com');
 
 
 \dn
-\dt esq_workflow.*
+\dt esq_almu_dena.*
