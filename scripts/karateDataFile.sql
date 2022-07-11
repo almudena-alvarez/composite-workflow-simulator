7d06cf3f6a59707acb525456a9cba9c9aad5397c
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-11 18:46:02 +0200

    Update karateDataFile.sql

--- scripts/karateDataFile.sql
+++ scripts/karateDataFile.sql
@@ -1,3 +1,25 @@
+CREATE TABLE :original_schema.first (
+	user_id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL
+);
+
+CREATE TABLE :original_schema.second (
+	user_id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL
+);
+
+CREATE TABLE :original_schema.third (
+	user_id serial PRIMARY KEY,
+	username VARCHAR ( 50 ) UNIQUE NOT NULL,
+	password VARCHAR ( 50 ) NOT NULL,
+	email VARCHAR ( 255 ) UNIQUE NOT NULL
+);
+
+
 INSERT INTO :original_schema.first VALUES
 (6, 'je', 'je', 'je@as.com'),
 (7, 'ja', 'ja', 'ja@bc.com'),
