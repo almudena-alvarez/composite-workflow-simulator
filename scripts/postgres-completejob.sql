55e42e346f053a46b12213f1585bb543dbab2bae
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-07-12 12:13:28 +0200

    Update postgres-completejob.sql

--- scripts/postgres-completejob.sql
+++ scripts/postgres-completejob.sql
@@ -1,4 +1,4 @@
-DROP SCHEMA $original_schema CASCADE;
+DROP SCHEMA :original_schema CASCADE;
 \dn;
-ALTER SCHEMA $copySchema RENAME TO $original_schema;
+ALTER SCHEMA :copy_schema RENAME TO :original_schema;
 \dn;
