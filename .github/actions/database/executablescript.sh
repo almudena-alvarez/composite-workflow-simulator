91c15765bfbe2172c674223bebc2ae6e851233e6
almudena-alvarez <98025287+almudena-alvarez@users.noreply.github.com>
2022-08-18 19:13:57 +0200

    Update executablescript.sh

--- .github/actions/database/executablescript.sh
+++ .github/actions/database/executablescript.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-echo "im in the executable script of the composite action"
 
-local prueba="esto es una prueba"
+prueba="esto es una prueba"
 echo "::set-output name=prueba::${prueba}"
 
